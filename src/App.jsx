193ab83dac41f1690e03e76a172fb3bd46408f5d
Yesukay <yesukay2@gmail.com>
2025-04-04 13:59:20 -0400

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function App() {
       </div>
 
 
-      <p className='.read-time'>Approx. reading time: 0 minutes</p>
+      <p className='read-time'>Approx. reading time: 0 minutes</p>
 
     </div>
 
